9a4a66021e7a818ce643614632d82e29ece5a1bc
e <liang.zhu@cn.dlink.com>
2020-12-14 18:25:44 +0800

    printErrorLog

--- NNMSProbe/src/main/service/log/LogService.java
+++ NNMSProbe/src/main/service/log/LogService.java
@@ -1,22 +1,31 @@
 package main.service.log;
 
 /**
- * Description: 日志服务<br> 
+ * Description: 鏃ュ織鏈嶅姟<br> 
  *  
  * @author GC <br>
  * @version 1.0 <br>
- * @CreateDate 2020年12月14日 <br>
+ * @CreateDate 2020骞�12鏈�14鏃� <br>
  * @since V1.0 <br>
  * @see main.service.log <br>
  */
 public interface LogService {
     
     /**
-     * Description: 打印日志<br> 
+     * Description: 鎵撳嵃鏃ュ織<br> 
      *  
      * @author GC<br>
      * @throws Exception <br>
      */
     void printLog() throws Exception;
+    
+    /**
+     * 
+     * Description: 打印错误日志<br> 
+     *  
+     * @author zl<br>
+     * @throws Exception <br>
+     */
+    void printErrorLog() throws Exception;
 
 }
